5951fb599b78dcb0c434732b3bb1a7a46ded75fa
SeniorDmitriy <dimaponomarenko443@gmail.com>
2022-04-22 18:18:20 +0300

    toggle todo complete

--- starter/src/components/TodoItem.js
+++ starter/src/components/TodoItem.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleComplete } from '../redux/todoSlice';
 
 const TodoItem = ({ id, title, completed }) => {
+	const dispatch = useDispatch();
+
+	const handlerCompleteClick = () => {
+		dispatch(
+			toggleComplete({ id: id, completed: !completed })
+		);
+	};
+
 	return (
-		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
-			<div className='d-flex justify-content-between'>
+		<li className={`list-group-item d-flex justify-content-between ${completed && 'list-group-item-success'}`}>
+			<label className='d-flex justify-content-between w-100'>
 				<span className='d-flex align-items-center'>
-					<input type='checkbox' className='mr-3' checked={completed}></input>
+					<input 
+					type='checkbox' 
+					className='mr-3' 
+					checked={completed}
+					onChange = {handlerCompleteClick}
+					></input>
 					{title}
 				</span>
-				<button className='btn btn-danger'>Delete</button>
-			</div>
+			</label>
+			<button className='btn btn-danger'>Delete</button>
 		</li>
 	);
 };
